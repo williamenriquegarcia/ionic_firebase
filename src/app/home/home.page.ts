ea4947ec334f2b4515f4321d43a44eded8a5874e
unknown <gasawi83@gmail.com>
2019-12-12 23:17:40 -0600

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  name;
+  last;
+  profession;
+  db;
+  
 
+  constructor() 
+  {
+    this.db=firebase.firestore();
+    
+  }
+
+  enviar()
+  {
+    console.log(this.name)
+    
+    this.db.collection("usuario").add({
+      nombre: this.name,
+      apellido: this.last,
+      ocupacion: this.profession
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+  }
+
+  
 }
